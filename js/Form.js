b9967fe5255d59e0771f293ab88bbf2707812cdc
Tejal-Page <71485773+Tejal-Page@users.noreply.github.com>
2020-12-11 16:37:21 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,9 +1,20 @@
+/*HINGS TO DO
+- CREATE TITLE HTML ELEMENT WITH H2 HEADER, POSITION IT
+- CREATE INPUT AND POSITION
+- CREATE BUTTON ELEMENT AND POSTION
+- DEFINE THINGS THAT HAPPEN WHEN U PRESS THE MOUSE
+    -HIDE BUTTON AND INPUT
+    -TAKE THE NAME AND STORE IT IN VAFR NAME
+    -INCREASE THE PLAYERCOUNT
+    -CREATE, DISPALY AND POSTION THE GREETING ELEMENT
+*/
 class Form {
-  constructor() {
+  constructor(){
     
   }
 
   display(){
+    //CRETING 
     var title = createElement('h2')
     title.html("Car Racing Game");
     title.position(130, 0);
